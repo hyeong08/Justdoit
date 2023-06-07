044940e8f47a5d805b8d4fd82611985634874b62
이규행 <2kyuhang@naver.com>
2023-06-07 20:05:42 +0900

    받은거 푸쉬..

--- app.py
+++ app.py
@@ -1,38 +1,53 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+import certifi
+ca = certifi.where()
+
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://sparta:test@cluster0.3tc0dmy.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://sparta:test@sparta.euiye0z.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
 db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/bucket", methods=["POST"])
-def bucket_post():
-    bucket_receive = request.form['bucket_give']
+# 저장
+@app.route("/todo", methods=["POST"])
+def todo_post():
+    todo_receive = request.form['todo_give']
 
-    bucket_list = list(db.bucket.find({}, {'_id': False}))
-    count = len(bucket_list) + 1
+    todo_list = list(db.todo.find({}, {'_id': False}))
+    count = len(todo_list) + 1
     doc = {
-        'num':count,  #버킷 등록 시, db에서 특정 버킷을 찾기 위해 'num' 이라는 고유 값 부여
-        'bucket' :bucket_receive,
-        'done' : 0   #'done' key값을 추가 해 각 버킷의 완료 상태 구분(0 = 미완료, 1 = 완료)
+        'num':count,
+        'todo' :todo_receive,
+        'done' : 0
     }
-    db.bucket.insert_one(doc)
-    return jsonify({'msg': '저장 완료!'})
+    db.todo.insert_one(doc)
+    return jsonify({'msg': '저장되었습니다!'})
 
-@app.route("/bucket/done", methods=["POST"])
-def bucket_done():
-	num_receive = request.form['num_give']
-	db.bucket.update_one({'num':int(num_receive)},{'$set':{'done':1}})
-	return jsonify({'msg': '버킷 완료!'})
-	
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
-	all_buckets = list(db.bucket.find({},{'_id':False}))	
-	return jsonify({'result': all_buckets})
+# 수정
+@app.route("/todo/update", methods=["POST"])
+def todo_update():
+    num_receive = int(request.form['num_give'])
+    todo_receive = request.form['todo_give']
+    
+    db.todo.update_one({'num':num_receive},{'$set':{'todo':todo_receive}})
+    return jsonify({'msg': '수정되었습니다!'})
+
+# 삭제
+@app.route("/todo/delete", methods=["POST"])
+def todo_delete():
+    num_receive = int(request.form['num_give'])
+    
+    db.todo.delete_one({'num':num_receive})
+    return jsonify({'msg': '삭제되었습니다!'})
+    
+@app.route("/todo", methods=["GET"])
+def todo_get():
+    all_todo = list(db.todo.find({},{'_id':False})) 
+    return jsonify({'result': all_todo})
 
 if __name__ == '__main__':
-		app.run('0.0.0.0', port=5000, debug=True)
+        app.run('0.0.0.0', port=5000, debug=True)
